dcb92f4f1f6de10635ef0123459cbb8c5211e087
Aaron Williams <awilliams@awillinc.com>
2018-09-05 23:30:42 -0400

    Minor formatting changes and fixing compiler warnings

--- app/src/main/java/com/awillinc/natetime/ClockActivity.java
+++ app/src/main/java/com/awillinc/natetime/ClockActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 //Testing testing GitHub 1 2 3. -Nate
 
@@ -30,7 +32,7 @@ public class ClockActivity extends AppCompatActivity { //this creates the CLASS 
         super.onCreate(savedInstanceState); //this entire line says to run the onCreate Activity IN ADDITION TO the existing Activity and not override the whole thing.
         setContentView(R.layout.activity_clock); //#aaroncreated in Android the visual design is created in xml . And each Activity is associated to a design. R means resource. layout means design. Aaron, you must have set a layout or design in activity_clock that you are calling here?
 
-        Button button = (Button) findViewById(R.id.refreshButton); //this looks like the opening line to adding the refresh button to the bottom of the screen and saying what to do when it's pressed. Why button three times? Each one fills a required
+        Button button = findViewById(R.id.refreshButton); //this looks like the opening line to adding the refresh button to the bottom of the screen and saying what to do when it's pressed. Why button three times? Each one fills a required
         button.setOnClickListener(new View.OnClickListener() { //this says to listen for the button to be clicked and
             public void onClick(View v) { //you did not make v. That looks like a user variable, but actually a v is the view that was clicked. Why have this whole onClick method? why not just have the updateTime method execute? probably because it does all the other standard stuff that happens when a button is clicked like change what it looks like?
                 updateTime();
@@ -43,7 +45,7 @@ public class ClockActivity extends AppCompatActivity { //this creates the CLASS 
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
@@ -55,7 +57,6 @@ public class ClockActivity extends AppCompatActivity { //this creates the CLASS 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
-                return;
             }
 
             // other 'case' lines to check for other
@@ -68,103 +69,84 @@ public class ClockActivity extends AppCompatActivity { //this creates the CLASS 
         Calendar c = Calendar.getInstance();
         Date currentTime = c.getTime();
 
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
         String formattedDate = df.format(currentTime); //#aaroncreated c and df. c is the current industrial time. df is a time/date format to work with. #aaroncreated formatteddate, which is the date in format df
         // formattedDate have current date/time
 
         // Now we display formattedDate value in TextView
-        TextView txtView = (TextView) findViewById(R.id.industrialTimeText); //#aaroncreated ... I KNOW you created industrialTimeText, but where did you define it? Are you defining it here? What
-        txtView.setText("Current Industrial Time: " + formattedDate); //#aaroncreated this text obviously
-
-        // Get the location
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        String provideGps = locationManager.GPS_PROVIDER;
-        String provideNetwork = locationManager.NETWORK_PROVIDER;
+        TextView txtView = findViewById(R.id.industrialTimeText); //#aaroncreated ... I KNOW you created industrialTimeText, but where did you define it? Are you defining it here? What
+        txtView.setText(String.format(Locale.US, "Current Industrial Time: %s", formattedDate)); //#aaroncreated this text obviously
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
+            // Why are we requesting fine location? Coarse should be enough.
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
             System.out.println("No location permissions");
             return;
         }
 
+        // Set the default location to be Columbus in case location detection fails
         double latitude = COLUMBUS_LATITUDE;
         double longitude = COLUMBUS_LONGITUDE;
 
-        // TODO: Use device location instead of hardcoded version
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        // Get the location
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
 
-        if (location == null) {
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
+            // Maybe switch network with gps to use less battery, and we don't need high precision
+            // see https://stackoverflow.com/questions/6775257/android-location-providers-gps-or-network-provider
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        if (location != null) {
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
+            if (location == null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
         }
 
         // Now we display locations value in TextView
-        TextView locationTxtView = (TextView) findViewById(R.id.locationText);
-        locationTxtView.setText(String.format("Location v3: %f° Lat, %f° Lon", latitude, longitude));
+        TextView locationTxtView = findViewById(R.id.locationText);
+        locationTxtView.setText(String.format(Locale.US,"Location: %f°, %f°", latitude, longitude));
 
         // Calculate sunrise and sunset. Source is https://github.com/caarmen/SunriseSunset
         Calendar[] sunriseSunset = ca.rmen.sunrisesunset.SunriseSunset.getSunriseSunset(c, latitude, longitude);
         Date sunrise = sunriseSunset[0].getTime();
         Date sunset = sunriseSunset[1].getTime();
 
-        // Convert to local daylight time, just so that sunrise sunset are always in the same day. TODO: Remove this.
-        // Date sunrise = sunrise - 4*60*60*1000;
-        // Date sunset = sunset - 4*60*60*1000;
-
         // Get the sunrise time
-        TextView sunriseTxtView = (TextView) findViewById(R.id.sunriseText);
+        TextView sunriseTxtView = findViewById(R.id.sunriseText);
         sunriseTxtView.setText(String.format("Sunrise: %tR", sunrise));
 
         // Get the sunset time
-        TextView sunsetTxtView = (TextView) findViewById(R.id.sunsetText);
+        TextView sunsetTxtView = findViewById(R.id.sunsetText);
         sunsetTxtView.setText(String.format("Sunset: %tR", sunset));
 
-        //TextView nateTimeTxtView = (TextView) findViewById(R.id.nateTimeText);
-
-
         // Calculate post-industrial time
-        TextView nateTimeTxtView = (TextView) findViewById(R.id.nateTimeText);
-        TextView dayLengthTxtView = (TextView) findViewById(R.id.dayLengthText);
+        TextView nateTimeTxtView = findViewById(R.id.nateTimeText);
+        TextView dayLengthTxtView = findViewById(R.id.dayLengthText);
 
         //Get the day length and display it.
         long dlms = sunset.getTime() - sunrise.getTime(); //day length in milliseconds.
         long dlSeconds = dlms / 1000 % 60;
         long dlMinutes = dlms / (60 * 1000) % 60;
         long dlHours = dlms / (60 * 60 * 1000);
-        dayLengthTxtView.setText(String.format("Day Length: %02d:%02d:%02d", dlHours, dlMinutes, dlSeconds));
+        dayLengthTxtView.setText(String.format(Locale.US,"Day Length: %02d:%02d:%02d", dlHours, dlMinutes, dlSeconds));
 
-
-        if (currentTime.before(sunrise))
-
-        {
+        if (currentTime.before(sunrise)) {
             long diff = sunrise.getTime() - currentTime.getTime();
             long diffSeconds = diff / 1000 % 60;
             long diffMinutes = diff / (60 * 1000) % 60;
             long diffHours = diff / (60 * 60 * 1000);
-            nateTimeTxtView.setText(String.format("Post-Industrial Time: %02d:%02d:%02d until sunrise", diffHours, diffMinutes, diffSeconds));
-        } else if (currentTime.before(sunset))
-
-        {
+            nateTimeTxtView.setText(String.format(Locale.US,"Post-Industrial Time: %02d:%02d:%02d until sunrise", diffHours, diffMinutes, diffSeconds));
+        } else if (currentTime.before(sunset)) {
             long diff = currentTime.getTime() - sunrise.getTime();
             long diffSeconds = diff / 1000 % 60;
             long diffMinutes = diff / (60 * 1000) % 60;
             long diffHours = diff / (60 * 60 * 1000);
-            nateTimeTxtView.setText(String.format("Post-Industrial Time: %02d:%02d:%02d after sunrise", diffHours, diffMinutes, diffSeconds));
-        } else
-
-        {
+            nateTimeTxtView.setText(String.format(Locale.US,"Post-Industrial Time: %02d:%02d:%02d after sunrise", diffHours, diffMinutes, diffSeconds));
+        } else {
             // Get tomorrow's sunrise time then calculate the time until sunrise
             Calendar tomorrow = Calendar.getInstance();
             // TODO: Test tomorrow being Jan 1
@@ -177,7 +159,7 @@ public class ClockActivity extends AppCompatActivity { //this creates the CLASS 
             long diffSeconds = diff / 1000 % 60;
             long diffMinutes = diff / (60 * 1000) % 60;
             long diffHours = diff / (60 * 60 * 1000);
-            nateTimeTxtView.setText(String.format("Post-Industrial Time: %02d:%02d:%02d until sunrise", diffHours, diffMinutes, diffSeconds));
+            nateTimeTxtView.setText(String.format(Locale.US,"Post-Industrial Time: %02d:%02d:%02d until sunrise", diffHours, diffMinutes, diffSeconds));
         }
     }
 }
